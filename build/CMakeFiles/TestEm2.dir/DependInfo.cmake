35b815b9f06cb669c964f9db4abd8425d75635f8
chandabindu <chandabindu@gmail.com>
2020-09-05 13:21:01 -0400

    modified code to include standard physics list

---
+++ build/CMakeFiles/TestEm2.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lead/G4WORK/TestEm2/TestEm2.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/TestEm2.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/ActionInitialization.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/ActionInitialization.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/DetectorConstruction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/DetectorConstruction.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/DetectorMessenger.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/DetectorMessenger.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/EmAcceptance.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/EmAcceptance.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/EventAction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/EventAction.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/PrimaryGeneratorAction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/Run.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/Run.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/RunAction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/RunAction.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/RunActionMessenger.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/RunActionMessenger.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/StepMax.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/StepMax.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/StepMaxMessenger.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/StepMaxMessenger.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/SteppingAction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/SteppingAction.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/SteppingVerbose.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/SteppingVerbose.cc.o"
+  "/home/lead/G4WORK/TestEm2/src/TrackingAction.cc" "/home/lead/G4WORK/TestEm2/build/CMakeFiles/TestEm2.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lead/product/clhep/clhep-install/lib/CLHEP-2.4.0.0/../../include"
+  "/home/lead/product/geant/geant-install/include/Geant4"
+  "../include"
+  "/home/lead/product/clhep/clhep-install/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/mkspecs/linux-g++"
+  "/usr/include/qt5/QtPrintSupport"
+  "/usr/include/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TestEm2.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestEm2.dir/TestEm2.cc.o"
+  "CMakeFiles/TestEm2.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/TestEm2.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/TestEm2.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/TestEm2.dir/src/EmAcceptance.cc.o"
+  "CMakeFiles/TestEm2.dir/src/EventAction.cc.o"
+  "CMakeFiles/TestEm2.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/TestEm2.dir/src/Run.cc.o"
+  "CMakeFiles/TestEm2.dir/src/RunAction.cc.o"
+  "CMakeFiles/TestEm2.dir/src/RunActionMessenger.cc.o"
+  "CMakeFiles/TestEm2.dir/src/StepMax.cc.o"
+  "CMakeFiles/TestEm2.dir/src/StepMaxMessenger.cc.o"
+  "CMakeFiles/TestEm2.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/TestEm2.dir/src/SteppingVerbose.cc.o"
+  "CMakeFiles/TestEm2.dir/src/TrackingAction.cc.o"
+  "TestEm2.pdb"
+  "TestEm2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestEm2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
